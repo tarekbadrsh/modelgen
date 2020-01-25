3a07e4e652df81eff6b959034ffcafcafea3509c
Tarek-Abdellatef <tarek@dowhile.se>
2020-01-25 23:42:16 +0100

    dicard lint

--- modelgen-concept/concept-api/api/CategoryAPI.go
+++ modelgen-concept/concept-api/api/CategoryAPI.go
@@ -9,11 +9,11 @@ import (
 )
 
 func configCategoriesRouter(routes *[]route) {
-	*routes = append(*routes, route{method: "GET", path: "/categories", handle: getAllCategories})
-	*routes = append(*routes, route{method: "POST", path: "/categories", handle: postCategories})
-	*routes = append(*routes, route{method: "PUT", path: "/categories", handle: putCategories})
-	*routes = append(*routes, route{method: "GET", path: "/categories/:id", handle: getCategories})
-	*routes = append(*routes, route{method: "DELETE", path: "/categories/:id", handle: deleteCategories})
+	*routes = append(*routes, route{method: "GET", path:"/categories", handle: getAllCategories})
+	*routes = append(*routes, route{method: "POST", path:"/categories", handle: postCategories})
+	*routes = append(*routes, route{method: "PUT", path:"/categories", handle: putCategories})
+	*routes = append(*routes, route{method: "GET", path:"/categories/:id", handle: getCategories})
+	*routes = append(*routes, route{method: "DELETE", path:"/categories/:id", handle: deleteCategories})
 }
 
 func getAllCategories(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -25,12 +25,13 @@ func getAllCategories(w http.ResponseWriter, r *http.Request, ps httprouter.Para
 }
 
 func getCategories(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-
+	
 	id, err := bll.ConvertCategoryID(ps.ByName("id"))
 	if err != nil {
 		http.Error(w, "Error: parameter (id) should be int32", http.StatusBadRequest)
 		return
 	}
+	
 
 	category, err := bll.GetCategory(id)
 	if err != nil {
@@ -71,13 +72,14 @@ func putCategories(w http.ResponseWriter, r *http.Request, ps httprouter.Params)
 }
 
 func deleteCategories(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-
+	
 	id, err := bll.ConvertCategoryID(ps.ByName("id"))
 	if err != nil {
 		http.Error(w, "Error: parameter (id) should be int32", http.StatusBadRequest)
 		return
 	}
-
+	
+	
 	err = bll.DeleteCategory(id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
